aa1110e85615520dcf767a0660258b24ad53f5cd
Olzhas Sutemgenov <olzhas.sutemgenov@nu.edu.kz>
2020-08-12 16:21:11 +0600

    presentational and container component

--- src/components/menuComponent.js
+++ src/components/menuComponent.js
@@ -4,39 +4,20 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 're
 class Menu extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            selectedDish: null
-        }
     }
 
-    onDishSelect(dish) {
-        this.setState({ selectedDish: dish});
+    componentDidMount() {
+       
+
     }
 
-    renderDish(dish) {
-        if (dish != null) {
-            return(
-                <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        }
-        else {
-            return(
-                <div></div>
-            );
-        }
-    }
+    
 
     render() {
         const menu = this.props.dishes.map((dish) => {
             return (
               <div key={dish.id} className="col-12 col-md-5 m-1">
-                <Card onClick={() => this.onDishSelect(dish)}>
+                <Card onClick={() => this.props.onClick(dish.id)}>
                     <CardImg width="100%" src={dish.image} alt={dish.name} />   
                     <CardImgOverlay>
                         <CardTitle>{dish.name}</CardTitle>
@@ -45,6 +26,7 @@ class Menu extends Component {
               </div>
             );
         });
+        console.log('Menu comp render invoked')
 
         return (
           <div className="container">
@@ -52,7 +34,6 @@ class Menu extends Component {
                   {menu}
             </div>
             <div className="row">
-                {this.renderDish(this.state.selectedDish)}
             </div>
           </div>
         );
